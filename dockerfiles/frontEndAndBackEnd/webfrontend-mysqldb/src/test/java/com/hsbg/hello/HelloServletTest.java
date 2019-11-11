869b26d58313b70b2aff9741cd7164818f3b8946
vilasvarghese <vilas.varghese@gmail.com>
2019-11-11 21:04:14 +0530

    add front end and back end integration

---
+++ dockerfiles/frontEndAndBackEnd/webfrontend-mysqldb/src/test/java/com/hsbg/hello/HelloServletTest.java
@@ -0,0 +1,41 @@
+package com.hsbg.hello;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+import static org.junit.Assert.*;
+
+
+
+public class HelloServletTest {
+	
+	
+	@Before
+	public void beforeEachTest() {
+		System.out.println("This is executed before each Test");
+	}
+
+	@After
+	public void afterEachTest() {
+		System.out.println("This is exceuted after each Test");
+	}
+
+	@Test
+	public void testHelloServlet() {
+		int result = 7;
+		assertEquals(7, result);
+	}
+}
+
