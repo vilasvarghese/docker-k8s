60293b7271d3343afc7d45df4a06fc3f678892e3
vilasvarghese <vilas.varghese@gmail.com>
2023-06-24 10:56:35 +0530

    changes

---
+++ yaml/test/curl-test.sh
@@ -0,0 +1,4 @@
+for i in {1..35}; do
+   kubectl exec --namespace=kube-public curl -- sh -c 'test=`wget -qO- -T 2  http://webapp-service.default.svc.cluster.local:8080/info 2>&1` && echo "$test OK" || echo "Failed"';
+   echo ""
+done
